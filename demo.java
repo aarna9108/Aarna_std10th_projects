9c2ab156f740b73c7bf4573091d8925b0cf108eb
aarna9108 <aarna9108@gmail.com>
2026-03-28 11:03:05 +0530

    Add files via upload

---
+++ demo.java
@@ -0,0 +1,14 @@
+package Test;
+
+public class demo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("My name is Aarna");
+		System.out.println("Studying in std 10th");
+		System.out.println("Roll no 1");
+		System.out.println("In school St. Francis");
+	}
+
+}

c4ab00beefc272d5ec5af1fc24b124808768c259
aarna9108 <aarna9108@gmail.com>
2026-07-21 14:12:10 +0530

    Add files via upload

---
+++ functin_find_area.java
@@ -0,0 +1,21 @@
+package java_project;
+
+public class functin_find_area {
+
+	public static void main(String[] args) {
+		
+		int product=area(10,12);
+		System.out.println("The area of the rectangle is "+product);
+		
+
+	}
+	
+	
+
+    public static int area(int a, int b) {
+    	
+    	return a*b;
+    }
+	
+	
+}
